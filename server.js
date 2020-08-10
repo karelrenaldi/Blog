acd75f0e26d0faf645fbcfc13a836bdf115db8e5
root <karelrenaldi8@gmail.com>
2020-08-10 12:06:18 +0000

    commit from server

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const app = require("./app");
-
-const port = 8080 || process.env.PORT;
+const fs = require('fs');
+const http = require('http');
+const https = require('https');
 
 dotenv.config({ path: "./config/config.env" });
 
@@ -18,6 +19,26 @@ const db = process.env.DATABASE;
   console.log("Database connection successful");
 })().catch(() => console.log("DB connection failed"));
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+const privateKey = fs.readFileSync('/etc/letsencrypt/live/swadayaganesha.com/privkey.pem', 'utf8');
+const certificate = fs.readFileSync('/etc/letsencrypt/live/swadayaganesha.com/cert.pem', 'utf8');
+const ca = fs.readFileSync('/etc/letsencrypt/live/swadayaganesha.com/chain.pem', 'utf8');
+
+const credentials = {
+	key: privateKey,
+	cert: certificate,
+	ca: ca
+};
+
+const httpServer = http.createServer(app);
+const httpsServer = https.createServer(credentials, app);
+
+httpServer.listen(80, () => {
+	console.log('HTTP Server running on port 80');
 });
+
+httpsServer.listen(443, () => {
+	console.log('HTTPS Server running on port 443');
+});
+
+
+
